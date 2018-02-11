779ba04ca5b9a3146198bca37bb6b60b3d285c06
Ru Chern Chong <me@ruchern.com>
2018-02-11 16:39:13 +0800

    Clean up default component and add new ones
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">Home</h1>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home,
+      meta: {
+        title: 'Home'
+      }
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+      meta: {
+        title: 'About'
+      }
     }
-  ]
+  ],
+  linkActiveClass: 'is-active'
 })
