3e9d673a37b45ec03290b1b5f5ac20aa1db7d7d1
Ru Chern Chong <me@ruchern.com>
2018-02-11 15:37:10 +0800

    Add Vuex config
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Task List`
+  next()
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
