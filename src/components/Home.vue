9c6c38e13d03d88707e80d48d98cb6197d363148
Ru Chern Chong <me@ruchern.com>
2018-02-11 20:19:57 +0800

    Add data and clean up
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,49 @@
     <section class="hero is-fullheight">
         <div class="hero-body">
             <div class="container">
-                <h1 class="title">Home</h1>
+                <h1 class="title">Task list</h1>
+
+                <search-task/>
+
+                <!--<div class="content">-->
+                <!--<strong>Filter by:</strong>-->
+
+                <!--<task-tag :key="tag.name" :tag="tag" v-for="tag in tags"/>-->
+                <!--</div>-->
+
+                <task-list/>
+                <new-task/>
             </div>
         </div>
     </section>
 </template>
 
 <script>
+  import SearchTask from '@/components/SearchTask'
+  import TaskTag from '@/components/TaskTag'
+  import TaskList from '@/components/TaskList'
+  import NewTask from '@/components/NewTask'
+
   export default {
-    name: 'home'
+    name: 'home',
+    components: {
+      SearchTask, TaskTag, TaskList, NewTask
+    },
+    data () {
+      return {
+        tags: [
+          {
+            name: 'All'
+          },
+          {
+            name: 'Completed'
+          },
+          {
+            name: 'Todo'
+          }
+        ]
+      }
+    }
   }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -25,7 +25,46 @@ export default new Vuex.Store({
         name: 'Stack Overflow',
         url: 'https://stackoverflow.com/users/4031163/ru-chern-chong'
       }
+    ],
+    tasks: [
+      {
+        id: 1,
+        name: 'Create a Vue demo for portfolio',
+        completed: true
+      },
+      {
+        id: 2,
+        name: 'Add tests to the demo',
+        completed: false
+      },
+      {
+        id: 3,
+        name: 'Add CircleCI Continuous Integration (CI)',
+        completed: false
+      }
+    ],
+    query: null
   },
-  mutations: {},
-  actions: {}
+  mutations: {
+    addTask (state, task) {
+      state.tasks.push(task)
+    },
+    setTaskCompleted (state, task) {
+      state.tasks[task.id - 1].completed = true
+    },
+    setQuery (state, query) {
+      state.query = query
+    }
+  },
+  actions: {
+    addTask ({commit}, task) {
+      commit('addTask', task)
+    },
+    setTaskCompleted ({commit}, task) {
+      commit('setTaskCompleted', task)
+    },
+    setQuery ({commit}, query) {
+      commit('setQuery', query)
+    }
+  }
 })
