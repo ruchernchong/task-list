7a2670aeb906d185a20c82df036f4c14ae0b4ec8
Ru Chern Chong <me@ruchern.com>
2018-02-13 01:13:02 +0800

    Set strict to development; resolves #5
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  strict: process.env.NODE_ENV === 'production',
+  strict: process.env.NODE_ENV !== 'production',
   state: {
     navs: [
       {
