888d8b2805f4c7667e1ef76ba398cc5cf8166a94
Ru Chern Chong <me@ruchern.com>
2018-02-13 03:05:35 +0800

    Add test for "NewTask" component
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ test/unit/specs/NewTask.spec.js
@@ -0,0 +1,56 @@
+import { createLocalVue, shallow } from '@vue/test-utils'
+import Vuex from 'vuex'
+import NewTask from '@/components/NewTask'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('NewTask', () => {
+  let wrapper
+
+  let actions
+  let store
+
+  beforeEach(() => {
+    actions = {
+      addTask: jest.fn()
+    }
+
+    store = new Vuex.Store({
+      state: {},
+      actions
+    })
+
+    wrapper = shallow(NewTask, {
+      store, localVue
+    })
+  })
+
+  test('message should be null when task name is not empty', () => {
+    const input = wrapper.find('input')
+    input.element.value = 'Task 1'
+    input.trigger('keyup')
+
+    expect(wrapper.vm.message).toBeNull()
+  })
+
+  test('add task when button is clicked', () => {
+    const input = wrapper.find('input')
+    input.element.value = 'Task 1'
+    input.trigger('input')
+
+    wrapper.find('button').trigger('click')
+
+    expect(actions.addTask).toHaveBeenCalled()
+  })
+
+  test('should display a message if task name is empty when button is clicked', () => {
+    const input = wrapper.find('input')
+    input.element.value = ''
+    input.trigger('input')
+
+    wrapper.find('button').trigger('click')
+
+    expect(wrapper.vm.message).toBe('Your task cannot be empty.')
+  })
+})
