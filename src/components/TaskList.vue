b934dede2a34b94885d3e92169f3ffcf8440bda1
Ru Chern Chong <me@ruchern.com>
2018-02-14 01:43:25 +0800

    Fix responsiveness on mobile
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="section">
-        <table class="table is-fullwidth">
-            <thead>
-            <tr>
-                <th>Status</th>
-                <th>Task</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
+    <div class="columns">
+        <div class="column">
+            <table class="table is-fullwidth">
+                <thead>
+                <tr>
+                    <th>Status</th>
+                    <th>Task</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
 
-            <tbody>
-            <tr :class="{ 'is-completed': task.completed }" :key="task.id" v-for="task in tasks">
-                <td class="has-text-centered">
-                    <i class="fa fa-2x fa-check-circle"
-                       :class="[ task.completed ? 'has-text-success' : 'has-text-muted' ]"
-                       @click="toggleTaskCompleted(task)"></i>
-                </td>
-                <td class="task-name" :class="{ 'is-completed': task.completed }"><strong>{{ task.name }}</strong></td>
-                <td>
-                    <task-action :task="task" @editTask="editTask" @openDialogModal="openDialogModal(task)"/>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+                <tbody>
+                <tr :class="{ 'is-completed': task.completed }" :key="task.id" v-for="task in tasks">
+                    <td class="has-text-centered">
+                        <i class="fa fa-2x fa-check-circle"
+                           :class="[ task.completed ? 'has-text-success' : 'has-text-muted' ]"
+                           @click="toggleTaskCompleted(task)"></i>
+                    </td>
+                    <td class="task-name" :class="{ 'is-completed': task.completed }"><strong>{{ task.name }}</strong></td>
+                    <td>
+                        <task-action :task="task" @editTask="editTask" @openDialogModal="openDialogModal(task)"/>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
 
-        <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-            <dialog-modal :key="isActive" :isActive="isActive" :task="task" @deleteTask="deleteTask"
-                          @closeDialogModal="closeDialogModal"/>
-        </transition>
+            <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+                <dialog-modal :key="isActive" :isActive="isActive" :task="task" @deleteTask="deleteTask"
+                              @closeDialogModal="closeDialogModal"/>
+            </transition>
+        </div>
     </div>
 </template>
 
