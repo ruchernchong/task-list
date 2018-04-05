18cf46c695a9e681bcead8b3a5ef4aae4d3b2295
Ru Chern Chong <me@ruchern.com>
2018-04-05 23:32:32 +0800

    Fix paths using `@`
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App'
-import store from './store'
-import router from './router'
+import App from '@/App'
+import store from '@/store'
+import router from '@/router'
 
 require('font-awesome/scss/font-awesome.scss')
 
