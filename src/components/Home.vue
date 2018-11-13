bf230d93bb9bec8ff5cdb3f65995fca89f2a874a
Ru Chern Chong <me@ruchern.com>
2018-11-14 00:04:30 +0800

    Add task footer
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,7 +17,7 @@
 
               <TaskList />
 
-              <footer class="card-footer"></footer>
+              <TaskFooter />
             </div>
 
             <!-- <div class="content"> -->
@@ -38,6 +38,7 @@ import Notification from '@/components/Notification'
 import SearchTask from '@/components/SearchTask'
 import TaskTag from '@/components/TaskTag'
 import TaskList from '@/components/TaskList'
+import TaskFooter from '@/components/TaskFooter'
 
 export default {
   name: 'Home',
@@ -45,15 +46,11 @@ export default {
     Notification,
     SearchTask,
     TaskTag,
-    TaskList
+    TaskList,
+    TaskFooter
   },
   computed: {
     ...mapState(['message'])
-  },
-  data () {
-    return {
-      tags: [{ name: 'All' }, { name: 'Completed' }, { name: 'Todo' }]
-    }
   }
 }
 </script>
@@ -61,18 +58,6 @@ export default {
 <style lang="scss" scoped>
 .card {
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
-}
-
-.card-footer {
-  content: '';
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 50px;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 8px 0 -3px #f6f6f6,
-    0 9px 1px -3px rgba(0, 0, 0, 0.2), 0 16px 0 -6px #f6f6f6,
-    0 17px 2px -6px rgba(0, 0, 0, 0.2);
 }
 
 .hero {
